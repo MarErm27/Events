16d37bfb00e22efe677707e0d277f75327585317
Marat Ermeshev <v27i72@gmail.com>
2020-01-23 20:21:11 +0300

    fix for Kit model issues

--- models/kit.go
+++ models/kit.go
@@ -9,7 +9,7 @@ type Kit struct {
 	uadmin.Model
 	Name           string
 	Nomenclature   Nomenclature
-	nomenclatureID uint
+	NomenclatureID uint
 	NumberInKit    int `uadmin:"min:1"`
-	CostInKit      int `uadmin:"money;pattern:^[0-9]*$;pattern_msg:Your input must be a number.;help:Input numeric characters only in this field.`
+	CostInKit      int `uadmin:"money;pattern:^[0-9]*$;pattern_msg:Your input must be a number.;help:Input numeric characters only in this field."`
 }
