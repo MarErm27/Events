11b81fce638b731e01418d8a9c01edc91bf34653
Marat Ermeshev <v27i72@gmail.com>
2020-01-23 21:32:10 +0300

    some work with menu model... Sum = Number *  Cost

--- models/menu.go
+++ models/menu.go
@@ -11,10 +11,38 @@ type Menu struct {
 	Event          Event
 	EventID        uint
 	Nomenclature   Nomenclature
-	nomenclatureID uint
+	NomenclatureID uint
 	Number         int `uadmin:"min:1"`
-	Cost           int `uadmin:"money;pattern:^[0-9]*$;pattern_msg:Your input must be a number.;help:Input numeric characters only in this field.`
-	Sum            int
+	Cost           int `uadmin:"money;pattern:^[0-9]*$;pattern_msg:Your input must be a number.;help:Input numeric characters only in this field."`
+	Sum            int `uadmin:"read_only"`
 	Kit            Kit
 	KitID          uint
+}
+
+// Save !
+func (m *Menu) Save() {
+	// Prints the result
+	uadmin.Trail(uadmin.DEBUG, "The document has changed.")
+	m.Sum = m.Number * m.Cost
+	// Save the document
+	uadmin.Save(m)
+	// Sets the document value to the DocumentVersion
+	summary := Menu{}
+	summary.Name = "Summary"
+	summary.Event = m.Event
+	summary.Number = m.Number
+	summary.Cost = m.Cost
+	summary.Sum = m.Sum
+
+	// Save the document version
+	uadmin.Save(&summary)
+}
+
+func (m Menu) Validate() (ret map[string]string) {
+	// ret = map[string]string{}
+	// if v.Name != "test" {
+	//     ret["Name"] = "Error name not found"
+	// }
+	m.Sum = m.Number * m.Cost
+	return
 }
